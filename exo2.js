ed72e921f150bd72f4397a557ecce447d16629ec
RomPri03 <romainprieur2810@gmail.com>
2024-11-20 11:15:06 +0100

    Exo2

---
+++ exo2.js
@@ -0,0 +1,10 @@
+//chaine de caractère
+var MavariableString = "toto";
+//nombre
+var MavariableNumerique = 100;
+//des tableaux
+var Mavariabletableau = ["Apple","Banana"];
+//des objects (ici la fenetre du navigateur)
+var MavariableObject = window
+
+alert(MavariableString + MavariableNumerique + Mavariabletableau[0] + MavariableObject.location.pathname);
