196a78513d313333cd0e6c89c7c7e2b8c5c333b2
RomPri03 <romainprieur2810@gmail.com>
2024-11-25 14:49:13 +0100

    Exo 7

---
+++ exo7.js
@@ -0,0 +1,10 @@
+let demande1=prompt("Entrez un nombre");
+let demande2=prompt("Entrez un nombre");
+
+demande1 = Number(demande1);
+demande2 = Number(demande2);
+
+let somme = demande1 + demande2;
+alert(somme);
+console.log(somme);
+
